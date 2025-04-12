3d0e6c1db35d9571877576c653e670167e245a22
Svalent99 <valen.sotelo.123@gmail.com>
2025-04-12 13:48:03 -0300

    Actualización de imágenes y modal

---
+++ android/app/src/main/java/com/valentina/vsdronshot/MainActivity.java
@@ -0,0 +1,5 @@
+package com.valentina.vsdronshot;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.valentina.vsdronshot',
+  appName: 'vsdronshotapp',
+  webDir: 'dist'
+};
+
+export default config;
